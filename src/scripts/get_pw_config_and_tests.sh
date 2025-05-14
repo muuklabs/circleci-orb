08f8fd2c20731e4d7483ff321d535162c40b2cd4
napoles <antonio.napoles@muuklabs.com>
2025-05-13 18:29:58 -0600

    Add baseurl parameter

--- src/scripts/get_pw_config_and_tests.sh
+++ src/scripts/get_pw_config_and_tests.sh
@@ -7,9 +7,15 @@ MUUK1_TOKEN=$(jq --raw-output .token token.json)
 printf "Authorization: Bearer %s" "$MUUK1_TOKEN" > header.txt
 MUUK_USERID_TOKEN=$(jq --raw-output .userId token.json)
 FIXED_TAG_VALUE=$MUUKTEST_TAG_VALUE
-[[ $FIXED_TAG_VALUE != "#"* ]] && FIXED_TAG_VALUE="#$FIXED_TAG_VALUE"
+if [[ "$MUUKTEST_TAG_PROPERTY" == "hashtag" ]]; then
+  [[ $FIXED_TAG_VALUE != "#"* ]] && FIXED_TAG_VALUE="#$FIXED_TAG_VALUE"
+fi
 printf '{"property": "%s", "value": ["%s"], "platform": "pw", "userId": "%s"}' "$MUUKTEST_TAG_PROPERTY" "$FIXED_TAG_VALUE" "$MUUK_USERID_TOKEN" > body.json
 curl -X POST https://portal.muuktest.com:8081/download_byproperty -H @header.txt -d @body.json -H "Content-Type: application/json" -o ./test.zip
 [ -d "./test" ] && rm -r test
 unzip -o test.zip -d ./test
+if [ "$BASE_TEST_URL" != "" ]; then
+  echo "Base URL: " $BASE_TEST_URL
+  sed -i '/let baseurl =/a\  baseurl = `'$BASE_TEST_URL'`;' ./test/TestSteps_*.spec.ts
+fi
 ls -l
