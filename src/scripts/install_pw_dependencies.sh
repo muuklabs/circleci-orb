5a0079ca60f24a9c81e024388ca07658628d3c9d
napoles <antonio.napoles@muuklabs.com>
2025-05-28 16:43:14 -0600

    Update 2.0.3

--- src/scripts/install_pw_dependencies.sh
+++ src/scripts/install_pw_dependencies.sh
@@ -5,9 +5,10 @@ sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
 NODE_MAJOR=20
 echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
-sudo apt-get update && sudo apt-get install nodejs -y
-echo "node -v" && node -v
-sudo apt-get update && sudo apt-get install gcc g++ make
-sudo apt-get update && sudo apt-get install npm
+sudo apt-get install -y \
+  nodejs \
+  gcc g++ make \
+  npm \
+  unzip \
+  ffmpeg
 echo "npm -v" && npm -v
-sudo apt-get update && sudo apt-get install -y unzip
